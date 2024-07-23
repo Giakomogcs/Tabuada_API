546386babb0ada5c95405a5271c6bd42bc647b94
Giovani <giakomogcs@gmail.com>
2024-07-22 21:36:29 -0300

    criando tabelas

--- src/database/knex/migrations/20240723003132_createUsers.ts
+++ src/database/knex/migrations/20240723003132_createUsers.ts
@@ -2,13 +2,11 @@ import { Knex } from "knex";
 
 export const up = async (knex: Knex): Promise<void> => {
   await knex.schema.createTable("users", (table) => {
-    table.increments("id").unsigned().primary();
+    table.text("id").unsigned().primary();
     table.text("name");
-    table.text("email");
-    table.text("password");
     table.text("picture");
-
-    table.timestamp("birthday");
+    table.text("class");
+    table.timestamp("age");
     table.timestamp("created_at");
     table.timestamp("updated_at");
   });
---
+++ src/database/knex/migrations/20240723003543_createAnswers.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+export const up = async (knex: Knex): Promise<void> => {
+  await knex.schema.createTable("answers", (table) => {
+    table.text("user_id").references("id").inTable("users").onDelete("CASCADE");
+
+    table.json("hits");
+
+    table.timestamp("created_at");
+    table.timestamp("updated_at");
+  });
+};
+
+export const down = async (knex: Knex): Promise<void> => {
+  await knex.schema.dropTable("answers");
+};
